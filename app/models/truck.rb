f2b7f9cbe71752f22946f68fc526f898d0cb7a9d
Sean Gee <seangee@gmail.com>
2014-04-14 14:45:04 -0700

    Association Dependencies

--- app/models/truck.rb
+++ app/models/truck.rb
@@ -1,5 +1,5 @@
 class Truck < ActiveRecord::Base
   attr_accessible :name, :address
-  has_many :dealerships
+  has_many :dealerships, dependent: :destroy
   has_many :truck_searches, :through => :dealerships
 end
--- app/models/truck_search.rb
+++ app/models/truck_search.rb
@@ -1,5 +1,5 @@
 class TruckSearch < ActiveRecord::Base
   attr_accessible :search
-  has_many :dealerships
+  has_many :dealerships, dependent: :destroy
   has_many :trucks, :through => :dealerships
 end

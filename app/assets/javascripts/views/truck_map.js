85b2c128860678f6dd504aa61aa38f7e1fcda608
Sean Gee <seangee@gmail.com>
2014-04-22 13:25:35 -0700

    Refactor map view

--- app/assets/javascripts/views/truck_map.js
+++ app/assets/javascripts/views/truck_map.js
@@ -5,28 +5,36 @@ window.FoodTruckFinder.Views.TruckMap = Backbone.View.extend({
    //  this.listenTo(this.model.trucks(), "sync remove", this.render);
   },
 
-  render: function(){
-    // Create Google map instance
-    var places = new Backbone.GoogleMaps.LocationCollection([{
-      title: "Off the Grid: 5M @ Fifth and Mina",
-      lat: 37.7824137,
-      lng: -122.4066171
-    }, {
-      title: "Off the Grid: UN Plaza",
-      lat: 37.7798761,
-      lng: -122.4139862
-    }]);
+  places: function () {
+    if (!this._places) {
+      this._places = new Backbone.GoogleMaps.LocationCollection([{
+        title: "Off the Grid: 5M @ Fifth and Mina",
+        lat: 37.7824137,
+        lng: -122.4066171
+      }, {
+        title: "Off the Grid: UN Plaza",
+        lat: 37.7798761,
+        lng: -122.4139862
+      }]);
+    }
+    return this._places
+  },
 
-    var map = new google.maps.Map($('#map_canvas')[0], {
+  map: function () {
+    if (!this._map) {
+      this._map = new google.maps.Map($('#map_canvas')[0], {
       center: new google.maps.LatLng(37.7822346, -122.4103306),
       zoom: 14,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     });
+    }
+    return this._map
+  },
 
-    // Render Markers
+  render: function(){
     var markerCollectionView = new Backbone.GoogleMaps.MarkerCollectionView({
-        collection: places,
-        map: map
+        collection: this.places(),
+        map: this.map()
     });
     markerCollectionView.render();
   }
---
+++ app/assets/javascripts/views/truck_searches_index.js
@@ -0,0 +1,21 @@
+window.FoodTruckFinder.Views.TruckSearchesIndex = Backbone.View.extend({
+  template: JST["truck_searches/index"],
+
+  initialize: function(options) {
+    this.listenTo(
+    	this.collection,
+    	"sync add",
+    	this.render
+    );
+  },
+
+  render: function() {
+  	var renderedContent = this.template({
+  		truck_searches: this.collection
+  	});
+  	this.$el.html( renderedContent );
+
+  	return this;
+  }, 
+
+});

094d2b589043c9806c67b67ba4df8984a9eabf3e
Sean Gee <seangee@gmail.com>
2014-04-24 02:43:57 -0700

    Add trucks to database

--- app/assets/javascripts/views/truck_searches_new.js
+++ app/assets/javascripts/views/truck_searches_new.js
@@ -19,23 +19,24 @@ window.FoodTruckFinder.Views.TruckSearchesNew = Backbone.View.extend({
 	submit: function (event) {
 		event.preventDefault();
 		var params = $(event.currentTarget).serializeJSON()["truck_search"];
-		var search = this.collection.where(params);
+    if (params.search.length == 0) {
+    	return
+    }
 
-		if (search.length == 0) {
+		var prevSearch = this.collection.where(params);
+
+		if (prevSearch.length == 0) {
 			var newTruckSearch = new FoodTruckFinder.Models.TruckSearch(params);
+			var that = this;
 			newTruckSearch.save({}, {
 				success: function () {
 					FoodTruckFinder.Collections.truckSearches.add(newTruckSearch);
+					that.codeAddress(params.search);
 					Backbone.history.navigate("", { trigger: true});
 				}
 			});
 		} else {
-			
-		}
-		// Call map's codeAddress
-		// TODO: move into call API section, and return DB info when no need for API.
-		if (params.search.length != 0){
-			this.codeAddress(params.search);
+			// Update map with DB info
 		}
 	},
 
@@ -44,8 +45,9 @@ window.FoodTruckFinder.Views.TruckSearchesNew = Backbone.View.extend({
     var geocoder = new google.maps.Geocoder();
     var that = this;
     var marker;
-    // var boundaries;
-    geocoder.geocode( { 'address': address}, function(results, status) {
+    var boundaries;
+    var list;
+    geocoder.geocode( { 'address': address }, function(results, status) {
       if (status == google.maps.GeocoderStatus.OK) {
         that.map.setCenter(results[0].geometry.location);
         if(marker != null){
@@ -58,9 +60,57 @@ window.FoodTruckFinder.Views.TruckSearchesNew = Backbone.View.extend({
       } else {
         alert('Geocode was not successful for the following reason: ' + status);
       }
-      // boundaries = getBoundaries(results);
-      // getTruckList(boundaries, address);
+      boundaries = that.getBoundaries(results);
+      that.saveTruckList(boundaries, address);
     });
-  }
+  },
+
+  // Calculate a box that is one mile in each direction of the geo coded coordinates.
+	// Using a one mile length for walking distance.
+	getBoundaries: function(geoData) {
+	  var boundaries = {};
+	  // one mile is roughly the same for around san francisco which is our dataset. 
+	  // However if we go outside of the city, one mile changes in lat lon. 
+	  var oneMileLat = 0.0145;
+	  var oneMileLon = 0.0183;
+	  var coordinates = [geoData[0].geometry.location.lat(), geoData[0].geometry.location.lng()];
+	  console.log("coordinates: " + coordinates);
+
+	  boundaries["north"] = coordinates[0] + oneMileLat;
+	  boundaries["east"] = coordinates[1] + oneMileLon;
+	  boundaries["south"] = coordinates[0] - oneMileLat;
+	  boundaries["west"] = coordinates[1] - oneMileLon;
+	  
+	  for (direction in boundaries){
+	    console.log("boundaries[" + direction + "] " + boundaries[direction]);
+	  }
+	  return boundaries;
+	},
+
+	// access the SF data given lat-lon boundaries and display the top ten results.
+	// It will show the Food Truck, address, and the food items.
+	saveTruckList: function(boundaries, address){
+		var truckItem = {}
+		var that = this;
+	  var sodaData = "http://data.sfgov.org/resource/rqzj-sfat.json?$limit=10&$where=within_box(location," + 
+	    boundaries["north"] + "," +
+	    boundaries["west"] + "," +
+	    boundaries["south"] + "," + 
+	    boundaries["east"] + ")"; 
+	  $.getJSON(sodaData, function(trucks){
+	    trucks.forEach( function(truck, idx){
+	    	truckItem["name"] = truck["applicant"];
+	      truckItem["address"] = truck["address"];
+	      truckItem["truck_search_id"] = FoodTruckFinder.Collections.truckSearches.last().get("id")
+
+	      var newTruck = new FoodTruckFinder.Models.Truck(truckItem)
+	      newTruck.save({}, {
+	      	success: function () {
+	      		FoodTruckFinder.Collections.truckSearches.last().trucks().add(newTruck);
+	      	}
+	      })
+	    });
+	  });
+	}
 
 });
